9f47318d748b2e4186049e6541afafc96aff1b60
Daniel Källstrand Modig <daniel@ikoncept.se>
2022-04-11 22:04:41 +0200

    Remove script references works too

--- post-build.js
+++ post-build.js
@@ -1,12 +1,9 @@
 const prettify = require('html-prettify');
 const fs = require ('fs')
 
-console.log('Cleaning up index.html...')
-
-const file = fs.readFileSync('./dist/index.html','utf-8')
-const output = file.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi,'')
-
-fs.writeFileSync('./dist/index.html', prettify(output), 'utf-8')
+// const file = fs.readFileSync('./dist/index.html','utf-8')
+// const output = file.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi,'')
+// fs.writeFileSync('./dist/index.html', prettify(output), 'utf-8')
 fs.rmSync('./dist/assets', { recursive: true, force: true })
 
-console.log('Writing new index.html and removing unused assets 🥲')
+console.log('Removing unused assets 🥲')
