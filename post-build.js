13dbf0126ae27b4c8595ed48cdcd666e5a0713ce
Daniel K Modig <daniel@ikoncept.se>
2023-04-04 15:17:08 +0200

    Add support for Alpine and inlining images

--- post-build.js
+++ post-build.js
@@ -1,5 +1,5 @@
 const prettify = require('html-prettify');
-const fs = require ('fs')
+const fs = require('fs')
 
 // const file = fs.readFileSync('./dist/index.html','utf-8')
 // const output = file.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi,'')
